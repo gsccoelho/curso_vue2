67a3f7040f865b6ed23b5b9780dcb0ca320f7e44
Guilherme Coelho <guilherme.coelho@unimedsm.com.br>
2023-06-09 11:42:38 -0300

    atualizando

--- componentes/componente-comunicacao/src/components/UsuarioInfo.vue
+++ componentes/componente-comunicacao/src/components/UsuarioInfo.vue
@@ -2,19 +2,39 @@
     <div class="componente">
         <h2>As Informações de Usuário</h2>
         <p>Vários detalhes...</p>
+        <p>Nome do Usuário: <strong> {{ inverterNome() }} </strong></p>        
     </div>
 </template>
 
 <script>
 export default {
-    
-}
+  props: {
+    usuarioNome: {
+      type: String,
+      required: true,
+      //default: "Anônimo",
+      default() {
+        return Array(10)
+          .fill(0)
+          .join(",");
+      },
+    },
+  },
+  methods: {
+    inverterNome() {
+      return this.usuarioNome
+        .split("")
+        .reverse()
+        .join("");
+    },
+  },
+};
 </script>
 
 <style scoped>
-    .componente {
-        flex: 1;
-        background-color: #ec485f;
-        color: #fff;
-    }
+.componente {
+  flex: 1;
+  background-color: #ec485f;
+  color: #fff;
+}
 </style>
